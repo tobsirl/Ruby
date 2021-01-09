24bc2dcff1b9283e401b7f6a590e7e8f6fb8d8e2
Paul Tobin <tobster111@gmail.com>
2021-01-09 14:04:31 +0000

    Printing to the console

---
+++ ruby-course/basics.rb
@@ -0,0 +1,8 @@
+# ruby followed by the file name will execute the code
+# comments
+# 3 ways to print to the console
+puts "Hello World"
+# p returns whatever arguement you give it
+p "Hello World"
+# print does not have a newline at the end
+print "Hello World"
