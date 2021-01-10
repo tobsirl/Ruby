b0c63055a1220c42c5b8078896bea26389cd6084
Paul Tobin <tobster111@gmail.com>
2021-01-10 12:20:53 +0000

    Getting input from the user with gets.chomp

--- ruby-course/basics/strings.rb
+++ ruby-course/basics/strings.rb
@@ -1,15 +1,20 @@
-# string concatenation
-sentence = "My name is Paul" 
-p sentence
+# # string concatenation
+# sentence = "My name is Paul" 
+# p sentence
 
-first_name = "Paul"
-last_name = "Tobin"
+# first_name = "Paul"
+# last_name = "Tobin"
 
-p first_name + " " + last_name
+# p first_name + " " + last_name
 
-# string interpolation
-puts "My first name is #{first_name} and my last name is #{last_name}"
+# # string interpolation
+# puts "My first name is #{first_name} and my last name is #{last_name}"
 
-# Methods 
-p first_name.class
-p 10.to_s
+# # Methods 
+# p first_name.class
+# p 10.to_s
+
+# Getting input from the user
+puts "What is your first name?"
+first_name = gets.chomp
+p "Hi #{first_name}, Welcome to ruby!"
