e9498311cdd78eb4135c5bf2c7480fe7113b2a4a
Paul Tobin <tobster111@gmail.com>
2021-01-14 18:44:57 +0000

    Comparing strings

--- ruby-course/basics/comparison_operators.rb
+++ ruby-course/basics/comparison_operators.rb
@@ -4,7 +4,12 @@
 # <
 
 # Examples
-p 10 == 9
-p 10 == 10
-p 10 == "10".to_i
-p 10 == "10".to_f
+# p 10 == 9
+# p 10 == 10
+# p 10 == "10".to_i
+# p 10 == "10".to_f
+
+p "hello" != "bye"
+p "hello" != "hello"
+p "hello" == "hello"
+
