12cb948cc98ec326998a895d144abaddb621927b
Paul Tobin <tobster111@gmail.com>
2021-01-14 18:58:52 +0000

    Comparing numbers

--- ruby-course/basics/comparison_operators.rb
+++ ruby-course/basics/comparison_operators.rb
@@ -9,7 +9,12 @@
 # p 10 == "10".to_i
 # p 10 == "10".to_f
 
-p "hello" != "bye"
-p "hello" != "hello"
-p "hello" == "hello"
+# p "hello" != "bye"
+# p "hello" != "hello"
+# p "hello" == "hello"
 
+p 100 > 99
+p 100 >= 100
+p 100 >= 99
+p 100 >= 101
+p 100 <= 101
