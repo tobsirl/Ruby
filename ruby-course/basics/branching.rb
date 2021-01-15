18a7dc85aadf53b6b2e4c7b5f6635abbdc5cbae3
Paul Tobin <tobster111@gmail.com>
2021-01-15 17:01:00 +0000

    Example of using elsif

--- ruby-course/basics/branching.rb
+++ ruby-course/basics/branching.rb
@@ -1,8 +1,17 @@
 # if/else
-condition = true
-another_condition = false
-if condition || another_condition
-  puts "hello"
+# condition = true
+# another_condition = false
+# if condition || another_condition
+#   puts "hello"
+# else
+#   puts "bye"
+# end
+
+name = "Paul"
+if name == "Paul"
+  puts "Welcome to the program, Paul"
+elsif name == "Jack"
+  puts "Welcome to the program, Jack"
 else
-  puts "bye"
+  puts "Welcome to the program, User"
 end
