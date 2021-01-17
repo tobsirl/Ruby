d900e405c949c722e0bd08cb05bdea91ae96b9ae
Paul Tobin <tobster111@gmail.com>
2021-01-17 10:52:32 +0000

    Example of select and each

--- ruby-course/basics/hash.rb
+++ ruby-course/basics/hash.rb
@@ -14,13 +14,13 @@ p another_hash[:a]
 # p sample_hash.values
 
 # Iterate over an hash
-sample_hash.each do |key, value|
-  puts "The class for key is #{key.class} and the value is #{value.class}"
-end
+# sample_hash.each do |key, value|
+#   puts "The class for key is #{key.class} and the value is #{value.class}"
+# end
 
-another_hash.each do |key, value|
-  puts "The class for key is #{key.class} and the value is #{value.class}"
-end
+# another_hash.each do |key, value|
+#   puts "The class for key is #{key.class} and the value is #{value.class}"
+# end
 
 # Adding to a hash
 myhash = {a: 1, b: 2, c: 3}
@@ -30,4 +30,6 @@ myhash[:e] = "Paul"
 myhash[:c] = "ruby"
 
 # iterate through a hash (shorthand)
-p myhash.each { |some_key, some_value| puts "The key is #{some_key} and the value is #{some_value}"}
+# p myhash.each { |some_key, some_value| puts "The key is #{some_key} and the value is #{some_value}"}
+p myhash.select { |k, v| v.is_a?(String) }
+p myhash.each { |k, v| myhash.delete(k) if v.is_a?(String) }
