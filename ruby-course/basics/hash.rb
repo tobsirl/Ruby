ce4527eef35de45fa755664781f2fa3e7856c865
Paul Tobin <tobster111@gmail.com>
2021-01-17 10:31:38 +0000

    Hashes using symbols

--- ruby-course/basics/hash.rb
+++ ruby-course/basics/hash.rb
@@ -1,5 +1,10 @@
-sample_hash = {'a' => 1, 'b' => 2, 'c' => 3}
-my_details = {'name' => 'paul', 'favcolor' => 'red'}
-p my_details['favcolor']
+# sample_hash = {'a' => 1, 'b' => 2, 'c' => 3}
+# my_details = {'name' => 'paul', 'favcolor' => 'red'}
+# p my_details['favcolor']
 
-Hash a.k.a. dictionary
+# Hash a.k.a. dictionary
+
+# Using symbols
+p another_hash = {a: 1, b: 2, c: 3}
+# Access using square brackets
+p another_hash[:a]
