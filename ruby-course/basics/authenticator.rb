5d42ea56216d435871d04781eeba00d4a4abd1ce
Paul Tobin <tobster111@gmail.com>
2021-01-18 18:56:15 +0000

    Authenticator app

---
+++ ruby-course/basics/authenticator.rb
@@ -0,0 +1,17 @@
+# users array where username and password are stored
+users = [
+  { username: "mashrur", password: "password1" },
+  { username: "jack", password: "password2" },
+  { username: "arya", password: "password3" },
+  { username: "jonshow", password: "password4" },
+  { username: "heisenberg", password: "password5" }
+]
+
+
+
+# program execution flow
+puts "Welcome to the authenticator"
+25.times { print "-" }
+puts
+puts "This program will take input from the user and compare password"
+puts "If password is correct, you will get back the user object"
