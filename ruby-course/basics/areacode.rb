a211ddd8dd98bd11636715373b0154f95a2433d2
Paul Tobin <tobster111@gmail.com>
2021-01-20 17:40:17 +0000

    Ruby coding challenge

---
+++ ruby-course/basics/areacode.rb
@@ -0,0 +1,30 @@
+dial_book = {
+  "newyork" => "212",
+  "newbrunswick" => "732",
+  "edison" => "908",
+  "plainsboro" => "609",
+  "sanfrancisco" => "301",
+  "miami" => "305",
+  "paloalto" => "650",
+  "evanston" => "847",
+  "orlando" => "407",
+  "lancaster" => "717"
+}
+ 
+# Get city names from the hash
+def get_city_names(somehash)
+# Write code here
+end
+ 
+# Get area code based on given hash and key
+def get_area_code(somehash, key)
+# Write code here
+end
+ 
+# Execution flow
+loop do input != 'Y' || input != 'y'
+# Write your program execution code here
+puts "Do you want to lookup an area code based on a city name?(Y/N)"
+input = gets.chomp
+if input == 'Y'
+end
