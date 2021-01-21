7af448aa41dcb57a1e091aaa7013b329e411805e
Paul Tobin <tobster111@gmail.com>
2021-01-21 15:52:16 +0000

    OOP - using attr_accessor

--- ruby-course/OOP/student.rb
+++ ruby-course/OOP/student.rb
@@ -1,4 +1,6 @@
 class Student
+  attr_accessor :first_name, :last_name, :email, :username
+
   @first_name
   @last_name
   @email
@@ -6,24 +8,24 @@ class Student
   @password
 
   # Setter
-  def first_name=(name)
-    @first_name = name
-  end
+  # def first_name=(name)
+  #   @first_name = name
+  # end
 
-  # Getter
-  def first_name
-    @first_name
-  end
+  # # Getter
+  # def first_name
+  #   @first_name
+  # end
 
-  # Setter last_name
-  def last_name=(last_name)
-    @last_name = last_name
-  end
+  # # Setter last_name
+  # def last_name=(last_name)
+  #   @last_name = last_name
+  # end
 
-  # Geter last_name
-  def last_name
-    @last_name
-  end
+  # # Geter last_name
+  # def last_name
+  #   @last_name
+  # end
 
   def to_s
     "First name: #{@first_name}"
@@ -33,5 +35,9 @@ end
 paul = Student.new
 paul.first_name = "Paul"
 paul.last_name = "Tobin"
+paul.email = "tobs@gmail.com"
+paul.username = "Toby"
 puts paul.first_name
 puts paul.last_name
+puts paul.email
+puts paul.username
