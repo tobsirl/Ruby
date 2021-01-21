9238aabcf2482df6859c495514e4755b6dd45644
Paul Tobin <tobster111@gmail.com>
2021-01-21 16:13:48 +0000

    OOP - using the initialize method

--- ruby-course/OOP/student.rb
+++ ruby-course/OOP/student.rb
@@ -1,6 +1,5 @@
 class Student
-  attr_accessor :first_name, :last_name, :email
-  attr_reader :username
+  attr_accessor :first_name, :last_name, :email, :username
 
   @first_name
   @last_name
@@ -8,41 +7,19 @@ class Student
   @username
   @password
 
-  def set_username
-    @username = "Toby"
+  def initialize(firstname, lastname, username, email, password)
+      @first_name = firstname
+      @last_name = lastname
+      @username = username
+      @email = email
+      @password = password
   end
-  # Setter
-  # def first_name=(name)
-  #   @first_name = name
-  # end
-
-  # # Getter
-  # def first_name
-  #   @first_name
-  # end
-
-  # # Setter last_name
-  # def last_name=(last_name)
-  #   @last_name = last_name
-  # end
-
-  # # Geter last_name
-  # def last_name
-  #   @last_name
-  # end
 
   def to_s
-    "First name: #{@first_name}"
+    "First name: #{@first_name}, Last name: #{@last_name}, Username: #{@username}, Email: #{@email}, Password: #{@password}"
   end
 end
 
-paul = Student.new
-paul.first_name = "Paul"
-paul.last_name = "Tobin"
-paul.email = "tobs@gmail.com"
-paul.set_username
+paul = Student.new("Paul", "Tobin", "Toby", "tobs@gmail.com", "pass1234")
 
-puts paul.first_name
-puts paul.last_name
-puts paul.email
-puts paul.username
+puts paul
