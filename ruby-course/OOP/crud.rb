e18621611b44bab1ec561bd65aeabe3f570f6694
Paul Tobin <tobster111@gmail.com>
2021-01-22 17:25:58 +0000

    Creating a verify method

--- ruby-course/OOP/crud.rb
+++ ruby-course/OOP/crud.rb
@@ -4,4 +4,13 @@ def create_hash_digest(password)
   BCrypt::Password.create(password)
 end
 
+def verify_hash_digest(password)
+  BCrypt::Password.new(password)
+end
+
 puts my_password = create_hash_digest("test")
+
+puts my_password == "test"
+puts my_password == "password"
+
+puts verify = verify_hash_digest(my_password)
