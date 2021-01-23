e518491f2e7327201a4ae5cff67de426a382da23
Paul Tobin <tobster111@gmail.com>
2021-01-23 12:18:20 +0000

    Using self for class level methods

--- ruby-course/OOP/crud.rb
+++ ruby-course/OOP/crud.rb
@@ -1,22 +1,22 @@
 module Crud
   require 'bcrypt'
 
-  def Crud.create_hash_digest(password)
+  def self.create_hash_digest(password)
     BCrypt::Password.create(password)
   end
   
-  def Crud.verify_hash_digest(password)
+  def self.verify_hash_digest(password)
     BCrypt::Password.new(password)
   end
   
-  def Crud.create_secure_users(list_of_users)
+  def self.create_secure_users(list_of_users)
     list_of_users.each do |user_record|
       user_record[:password] = create_hash_digest(user_record[:password])
     end
     list_of_users
   end
   
-  def Crud.authenticate_user(username, password, list_of_users)
+  def self.authenticate_user(username, password, list_of_users)
     list_of_users.each do |user_record|
       if user_record[:username] == username && verify_hash_digest(user_record[:password]) == password
         return user_record
